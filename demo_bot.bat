d4e46d4c67cac3da0b3566b54b930ea12c5067ff
gersonfreire <gerson.freire@gmail.com>
2024-08-29 11:59:43 -0300

    Refactor demo_bot.bat to activate virtual environment and run the bot

--- demo_bot.bat
+++ demo_bot.bat
@@ -2,10 +2,10 @@
 @REM This script is used to run the bot in the Windows environment
 
 @REM Go to current directory
-cd %~dp0\..\..
+cd %~dp0
 
 @REM Activate the virtual environment and run the bot
 call .venv\Scripts\Activate
 
 @REM Run the bot script repassing the arguments
-python src\cnpj_bot\tlgfwk.py %*
+python tlgfwk.py %*
