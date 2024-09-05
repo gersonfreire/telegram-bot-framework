b15787407edfc8233c64b8c35c79e5b6fdd28592
gersonfreire <gerson.freire@gmail.com>
2024-09-04 21:50:12 -0300

    chore: Bump version to 0.2.6 and save admin list to .env

--- tlgfwk.py
+++ tlgfwk.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-version = '0.2.5 Save admin list to .env'
+version = '0.2.6 Save admin list to .env'
 
 # ------------------------------------------
 
