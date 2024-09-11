90c37c0072260656d24d398af8a98617800134b8
gersonfreire <gerson.freire@gmail.com>
2024-09-11 11:07:20 -0300

    Refactor code structure: Add admin command to show users from persistence file

--- tlgfwk.py
+++ tlgfwk.py
@@ -7,34 +7,6 @@ __version__ = '0.3.7 Added command to show users from persistence file'
 from __init__ import *
 import translations.translations as translations
 
-# async def cmd_show_users(self, update: Update, context: CallbackContext):
-#     """Show the users from the persistence file
-
-#     Args:
-#         update (Update): The update object
-#         context (CallbackContext): The callback context
-#     """
-#     try:
-#         # Get the user dictionary from the bot data
-#         user_dict = context.bot_data.get('user_dict', {})
-        
-#         # Check if there are any users in the dictionary
-#         if user_dict:
-#             # Create a list of user names
-#             user_names = [user.username for user in user_dict.values()]
-            
-#             # Create a message with the user names
-#             message = "Users from persistence file:\n" + "\n".join(user_names)
-#         else:
-#             message = "No users found in the persistence file."
-        
-#         # Send the message to the user
-#         await update.message.reply_text(message)
-        
-#     except Exception as e:
-#         logger.error(f"Error in cmd_show_users: {e}")
-#         await update.message.reply_text(f"Sorry, we encountered an error: {e}")
-
 class TlgBotFwk(Application):
     
     # ------------- util functions ------------------
@@ -690,6 +662,36 @@ _Links:_
             return f'Sorry, we have a problem sending message: {e}'
        
     # -------- Default command handlers --------
+    
+    @with_writing_action
+    @with_log_admin
+    async def cmd_show_users(self, update: Update, context: CallbackContext):
+        """Show the users from the persistence file
+
+        Args:
+            update (Update): The update object
+            context (CallbackContext): The callback context
+        """
+        try:
+            # Get the user dictionary from the bot data
+            user_dict = context.bot_data.get('user_dict', {})
+            
+            # Check if there are any users in the dictionary
+            if user_dict:
+                # Create a list of user names
+                user_names = [user.username for user in user_dict.values()]
+                
+                # Create a message with the user names
+                message = "Users from persistence file:\n" + "\n".join(user_names)
+            else:
+                message = "No users found in the persistence file."
+            
+            # Send the message to the user
+            await update.message.reply_text(message)
+            
+        except Exception as e:
+            logger.error(f"Error in cmd_show_users: {e}")
+            await update.message.reply_text(f"Sorry, we encountered an error: {e}")    
     
     @with_writing_action
     @with_log_admin
