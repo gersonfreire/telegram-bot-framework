5c202356046307b2374814003dd005c98f954a4d
gersonfreire <gerson.freire@gmail.com>
2024-09-12 10:57:17 -0300

    Refactor code structure: Update imports in util_decorators.py, util_env.py, and util_log.py

--- util/util_env.py
+++ util/util_env.py
@@ -3,17 +3,20 @@
 # pylint: disable=unused-argument
 
 # ------------------------------------------
+import os
 from dotenv import * # load_dotenv
+
+from util_log import *
 # ------------------------------------------
 
-try:
-    from util_config import *
-except ImportError:
-    try:
-        from util.util_config import *
-    except ImportError:
-        print("Error: util_config.py not found or import error.")
-        exit(1)
+# try:
+#     from util_config import *
+# except ImportError:
+#     try:
+#         from util.util_config import *
+#     except ImportError:
+#         print("Error: util_config.py not found or import error.")
+#         exit(1)
 
 # ------------------------------------------
 
