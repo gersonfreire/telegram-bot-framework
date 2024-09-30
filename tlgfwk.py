1b41bfe66ea445112e0e398f1665030282d3db4e
gersonfreire <gerson.freire@gmail.com>
2024-09-30 00:03:04 -0300

    Refactor code structure: Update version number and change log in tlgfwk.py

--- tlgfwk.py
+++ tlgfwk.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------
 
-__version__ = """0.7.7 change payment tokens"""
+__version__ = """0.7.8 Add user into the user list from the annotation of the command handler"""
 __change_log__ = """
 TODO´s:
 0.6.3 Load just a specified plugin
@@ -18,6 +18,7 @@ TODO´s:
 0.7.4 Fixed the show balance command and show all user´s data
 0.7.5 Migrate user´s balance storage on the user data context 
 0.7.6 Fixed duplicate commands 
+0.7.7 Change payment tokens
 """
 
 from __init__ import *
