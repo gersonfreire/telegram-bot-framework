206de7ee24562d5dceba2c026106d2baaf8082ab
gersonfreire <gerson.freire@gmail.com>
2024-10-03 18:52:54 -0300

    Refactor PayPal payment creation and add main function to util_paypal_receive.py

--- util/util_paypal_receive.py
+++ util/util_paypal_receive.py
@@ -5,7 +5,7 @@ import paypalrestsdk
 dotenv.load_dotenv()
 
 client_id = os.getenv("PAYPAL_CLIENT_ID")
-client_secret = os.getenv("PAYPAL_CLIENT_SECRET")
+client_secret = os.getenv("PAYPAL_CLIENT_SECRET")   
 
 app = Flask(__name__)
 
@@ -15,6 +15,64 @@ paypalrestsdk.configure({
     "client_id": client_id,
     "client_secret": client_secret
 })
+
+# --------------------------------
+
+@app.route('/payment/link', methods=['GET'])
+def create_payment(
+    return_url="http://localhost:5000/payment/execute", 
+    cancel_url="http://localhost:5000/payment/cancel", 
+    total="5.00", 
+    currency="BRL", 
+    description="This is the payment transaction description.",
+    client_id = os.getenv("PAYPAL_CLIENT_ID") ,
+    client_secret = os.getenv("PAYPAL_CLIENT_SECRET")
+    ): 
+
+    try:  
+
+        paypalrestsdk.configure({
+            "mode": "sandbox",  # sandbox or live
+            "client_id": client_id,
+            "client_secret": client_secret
+        })
+        
+        # Step 3: Create a Payment
+        payment = paypalrestsdk.Payment({
+            "intent": "sale",
+            "payer": {
+                "payment_method": "paypal"},
+            "redirect_urls": {
+                "return_url": return_url,
+                "cancel_url": cancel_url},
+            "transactions": [{
+                "item_list": {
+                    "items": [{
+                        "name": "item",
+                        "sku": "item",
+                        "price": "5.00",
+                        "currency": "BRL",
+                        "quantity": 1}]},
+                "amount": {
+                    "total": total,
+                    "currency": currency},
+                "description": description}]})
+
+        # Step 4: Generate the Payment Link
+        if payment.create():
+            print("Payment created successfully")
+            for link in payment.links:
+                if link.rel == "approval_url":
+                    approval_url = str(link.href)
+                    print("Redirect for approval: %s" % (approval_url))
+                    
+        else:
+            print(payment.error)
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# -------------------------------- 
 
 @app.route('/payment/execute', methods=['GET'])
 def execute_payment():
@@ -35,7 +93,10 @@ def cancel_payment():
     return "Payment cancelled"
 
 def main(debug=True):
-    app.run(debug)
+    
+    create_payment()
+    
+    app.run(debug=False)
 
 if __name__ == '__main__':
     main()
