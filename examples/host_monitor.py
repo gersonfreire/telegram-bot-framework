08028fe2712202b260f3131a14bc6221b3a4921d
gersonfreire <gerson.freire@gmail.com>
2024-10-15 08:44:39 -0300

    Refactor host_monitor.py to add host monitoring functionality

--- examples/host_monitor.py
+++ examples/host_monitor.py
@@ -1,6 +1,7 @@
 import os
 import time
 import schedule
+from tlgfwk import TlgBotFwk
 
 def ping_host(ip_address):
     response = os.system(f"ping -c 1 {ip_address}")
@@ -9,8 +10,29 @@ def ping_host(ip_address):
     else:
         print(f"{ip_address} is down!")
 
-def job():
-    ping_host("8.8.8.8")
+class HostMonitorBot(TlgBotFwk):
+    def __init__(self, ip_address):
+        super().__init__()
+        self.ip_address = ip_address
+
+    def job(self):
+        self.ping_host(self.ip_address)
+
+    def ping_host(self, ip_address):
+        response = os.system(f"ping -c 1 {ip_address}")
+        if response == 0:
+            print(f"{ip_address} is up!")
+        else:
+            print(f"{ip_address} is down!")
+
+# Create an instance of the bot
+bot = HostMonitorBot("8.8.8.8")
+
+# Schedule the job every 10 minutes using the bot's scheduling method
+bot.schedule.every(10).minutes.do(bot.job)
+
+# Start the bot's main loop
+bot.run()
 
 # Schedule the job every 10 minutes
 schedule.every(10).minutes.do(job)
