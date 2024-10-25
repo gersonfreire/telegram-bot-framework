9e7313384d585942d669351e7086a457e87d29c7
gerson <gerson.freire@gmail.com>
2024-10-25 14:59:10 -0300

    Refactor code to update version number and fix unknown commands message

--- examples/host_watch_bot.py
+++ examples/host_watch_bot.py
@@ -8,11 +8,11 @@ overrides the initialize_handlers method to add a help command handler.
 This version is inspired on and more elaborated than host_monitor because controls each host by user.
 """
 
-__version__ = '0.4.6 Enable and fix unknown commands "ainda não foi implementado" message'
+__version__ = '0.4.7 Enable and fix unknown commands "ainda não foi implementado" message'
 
-# DOING: Enable and fix unknown commands "ainda não foi implementado" message
+# DONE: Enable and fix unknown commands "ainda não foi implementado" message
 # DONE: Open URL links at internal telegram browser, it is enough to format the URL as a markdown link
-# TODO: Improve pinglist message formatting with header and table
+# DONE: Improve pinglist message formatting with header and table
 # TODO: Pagination
 
 import __init__
