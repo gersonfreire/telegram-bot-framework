5667670658db80a27b47d89bc904f8bda1922505
gerson <gerson.freire@gmail.com>
2024-10-30 09:16:03 -0300

    Add create_test_checkout_session function and refactor create_checkout_session parameters

--- pocs/stripe/link_console.py
+++ pocs/stripe/link_console.py
@@ -10,12 +10,7 @@ load_dotenv()
 # Set your secret key. Remember to switch to your live secret key in production!
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 
-def create_checkout_session(
-    currency= 'brl',
-    success_url='https://yourdomain.com/success?session_id={CHECKOUT_SESSION_ID}',
-    cancel_url='https://yourdomain.com/cancel',
-    
-):
+def create_test_checkout_session():
     try:
         session = stripe.checkout.Session.create(
             payment_method_types=['card'],
@@ -38,8 +33,41 @@ def create_checkout_session(
         print(f"An error occurred: {e}")
         return None
 
+def create_checkout_session(
+    payment_method_types=['card'],
+    currency= 'brl',
+    product_name = 'Credito RFB Bot',
+    unit_amount = 2000,
+    quantity = 1,
+    mode='payment',
+    success_url='https://yourdomain.com/success?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url='https://yourdomain.com/cancel',
+    
+):
+    try:
+        session = stripe.checkout.Session.create(
+            payment_method_types=payment_method_types,
+            line_items=[{
+                'price_data': {
+                    'currency': currency,
+                    'product_data': {
+                        'name': product_name,
+                    },
+                    'unit_amount': unit_amount,  # Amount in cents
+                },
+                'quantity': quantity,
+            }],
+            mode='payment',
+            success_url='https://yourdomain.com/success?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url='https://yourdomain.com/cancel',
+        )
+        return session.url
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
 if __name__ == "__main__":
-    payment_link = create_checkout_session()
+    payment_link = create_test_checkout_session()
     if payment_link:
         print(f"Checkout payment link: {payment_link}")
     else:
