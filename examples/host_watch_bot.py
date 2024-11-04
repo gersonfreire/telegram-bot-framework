f1c91cea5c3d32ba8e53f21bdd9ae9816983052b
gerson <gerson.freire@gmail.com>
2024-11-04 14:30:28 -0300

    Implement port check in HostWatchBot ping method

--- examples/host_watch_bot.py
+++ examples/host_watch_bot.py
@@ -231,8 +231,8 @@ class HostWatchBot(TlgBotFwk):
             http_ping_result = await self.http_ping(job_param, debug_status=show_success, user_id=user_id, http_type='http')
             
             # TODO: execute a check for a specific port
-            # port = 80
-            # port_result = await self.ping_host_port(job_param, port, show_success=show_success, user_id=user_id)
+            port = 80
+            port_result = await self.ping_host_port(job_param, port, show_success=show_success, user_id=user_id)
             
             job_name = f"ping_{job_param}"  
             callback_context.user_data[job_name]['last_status'] = ping_result # and http_ping_result
