47e91274b33243ed2d639107ab54c1ad9c3f7705
gerson <gerson.freire@gmail.com>
2024-11-05 17:00:09 -0300

    Add Telegram bot and web app for user interaction

---
+++ pocs/miniapp/webapp.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template_string('''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Telegram Web App</title>
+        </head>
+        <body>
+            <h1>Enter your name</h1>
+            <form action="/submit" method="post">
+                <label for="name">Your name:</label>
+                <input type="text" id="name" name="name">
+                <input type="submit" value="Submit">
+            </form>
+        </body>
+        </html>
+    ''')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f'<script>window.Telegram.WebApp.sendData("{name}");</script>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
