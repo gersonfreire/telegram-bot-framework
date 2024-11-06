08a9d9d1ee11873b6ddb08a285e7c4e0fbd1e81f
gerson <gerson.freire@gmail.com>
2024-11-06 16:53:07 -0300

    Refactor sys_path.py to remove hardcoded site-packages path and import tlgfwk module

--- pocs/sys_path.py
+++ pocs/sys_path.py
@@ -3,8 +3,8 @@ import os
 
 # Adicione o caminho da pasta raiz do script ao path do python
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-sys.path.append(r"c:\Users\gerso\source\repo\gerson\novo-cnpj\.venv\Lib\site-packages")
-import tlgfwk
+# sys.path.append(r"c:\Users\gerso\source\repo\gerson\novo-cnpj\.venv\Lib\site-packages")
+from tlgfwk import *
 
 # Verifique o sys.path
 print(sys.path)
